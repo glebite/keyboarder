c82dc9115922f8f2203f5f124d8c8d6a7a405a3a
glebite <glebite@gmail.com>
2021-09-26 08:51:25 -0400

    Adding in signal handler to deal with C-c and mishandling of screen cleanup...

--- src/layout.py
+++ src/layout.py
@@ -2,22 +2,31 @@
 layout.py
 """
 import curses
+import signal
+import time
 
 
 class Layout(object):
     """
     """
     def __init__(self):
-        self.keys = curses.initscr()
+        self.screen = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        self.screen.clear()
+        time.sleep(10)
+
+    def display_keys(self):
+        pass
 
     def __del__(self):
         curses.endwin()
 
 
 def main():
-    pass
+    keys = Layout()
+    signal.signal(signal.SIGINT, keys.__del__)
+    keys.display_keys()
 
 
 if __name__ == "__main__":
