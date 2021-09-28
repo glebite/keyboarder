61f35b0b385eaf3d814dcd3dde34ffc9b75a6de9
glebite <glebite@gmail.com>
2021-09-27 21:54:07 -0400

    removed farsi for now - will add to a new format soon

--- src/layout.py
+++ src/layout.py
@@ -5,6 +5,16 @@ import curses
 import signal
 import time
 
+English = dict()
+English['lower'] = {'` 1 2 3 4 5 6 7 8 9 0 - = ',
+                    'q w e r t y u i o p [ ] \' ',
+                    'a s d f g h j k  l ; \\ ',
+                    'z x c v b n m , . / '}
+English['upper'] = {'~ ! @ # $ % ^ & * ( ) _ + ',
+                    'Q W E R T Y U I O P { } | ',
+                    'A S D F G H J K L : \"',
+                    'Z X C V B N M < > ? '}
+
 
 class Layout(object):
     """
