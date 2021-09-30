6494b8ec328946f50cae73ea1846876ad87675fe
glebite <glebite@gmail.com>
2021-09-29 21:41:02 -0400

    Fixed it up - should start to be good to compare keys and such.

--- src/layout.py
+++ src/layout.py
@@ -7,16 +7,24 @@ import time
 from array import *
 
 English = dict()
-English['lower'] = {'` 1 2 3 4 5 6 7 8 9 0 - = ',
-                    'q w e r t y u i o p [ ] \' ',
-                    'a s d f g h j k  l ; \\ ',
-                    'z x c v b n m , . / '}
-English['upper'] = {'~ ! @ # $ % ^ & * ( ) _ + ',
-                    'Q W E R T Y U I O P { } | ',
-                    'A S D F G H J K L : \"',
-                    'Z X C V B N M < > ? '}
+English['lower'] = ['`1234567890-=',
+                    'qwertyuiop[]\'',
+                    'asdfghjkl;\\',
+                    'zxcvbnm,./']
+English['upper'] = ['~!@#$%^&*()_+',
+                    'QWERTYUIOP{}|',
+                    'ASDFGHJKL:\"',
+                    'ZXCVBNM<>?']
 
-Farsi = ['‍', '۱', '۲', '۳', '۴', '۵','۶', '۷', '۸', '۹', '۰', '-' , '=']
+Farsi = dict()
+Farsi['lower'] = ['‍۱۲۳۴۵۶۷۸۹۰-=',
+                  'ضصثقفغعهخحجچ\\',
+                  'شسیبلاتنمکگ',
+                  'ظطزرذدپو./']
+Farsi['upper'] = ['÷!٬٫﷼٪×،*)(ـ+',
+                  'ًٌٍَُِّْ][}{|',
+                  'ؤئيإأآة»«:؛',
+                  'كٓژٰ‌ٔء><؟']
 
 
 class Layout(object):
