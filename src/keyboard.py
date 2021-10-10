bcf405a66ed7da7f64ccb313a92481bc5531e056
glebite <glebite@gmail.com>
2021-10-10 13:11:33 -0400

    Starting definition of keyboard mapping and data files.

---
+++ src/keyboard.py
@@ -0,0 +1,12 @@
+import key
+import csv
+
+
+class keyboard(object):
+    def __init__(self, key_file):
+        self.key_file = key_file
+        self.layout = array()
+        
+    def read_config(self):
+
+            
