94fe31989990d42fb675ce0b8104a36163ed3e4c
glebite <glebite@gmail.com>
2021-10-11 05:39:46 -0400

    Cleanup - preparing to build the structure for the keyboard.

--- src/key.py
+++ src/key.py
@@ -1,44 +1,51 @@
 class Key(object):
     """Key - definition of a Key class.
 
-    This will allow for the addressing of a key based on 
+    This will allow for the addressing of a key based on
     its row, column, whether or not it should be displayed, or what
     to display when it's either upper case or lower case.
     """
-    def __init__(self, row, column, visibility, cases):
+    def __init__(self, row, column, visibility, lower, upper):
         """__init__ - constructor
 
         Params:
         row - int - keyboard row (0 < row < n)
         column - int - keyboard column (0 < column < m)
         visibility - int - whether or not the key is to be displayed
-        cases - dict:
-                {'lower' : 'a',
-                 'upper' : 'A'}
+        lower - int - lower case characters
+        upper - int - upper case characters
         Returns:
         n/a
 
         Raises:
         n/a
         """
-        assert 0 < row < 25, f'{row} out of 0<row<25 bounds'
-        assert 0 < column < 80, f'{column} out of 0<column<25 bounds'
+        row = int(row)
+        column = int(column)
+        visibility = int(visibility)
+
+        assert 0 <= row < 25, f'{row} out of 0<row<25 bounds'
+        assert 0 <= column < 80, f'{column} out of 0<column<25 bounds'
         self.row = row
         self.column = column
         self.visibility = visibility
-        self.cases = cases
+        self.lower = lower
+        self.upper = upper
 
     def __str__(self):
-        return f'{self.row} {self.column} {self.visibility} {self.cases}'
+        return f'{self.row} {self.column}' \
+            f' {self.visibility} {self.lower} {self.upper}'
 
 
 def main():
     try:
-        x = Key(39, 10, 1, {'lower': 'a', 'upper': 'A'})
+        x = Key(39, 10, 1, 'a', 'A')
         print(f'{x}')
     except AssertionError as e:
         print(f'Caught expected exception {e}')
-    x = Key(10, 10, 1, {'lower': 'a', 'upper': 'A'})
+    except Exception as e:
+        print(f'Caught unexpected exception {e}')
+    x = Key(10, 10, 1, 'a', 'A')
     print(f'{x}')
 
 
--- src/keyboard.py
+++ src/keyboard.py
@@ -1,12 +1,31 @@
-import key
+from key import Key
 import csv
 
 
 class keyboard(object):
     def __init__(self, key_file):
         self.key_file = key_file
-        self.layout = array()
-        
-    def read_config(self):
+        self.layout = list()
 
-            
+    def read_config(self):
+        temp_row = list()
+        cur_row = -1
+        with open(self.key_file, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='\'')
+            for row in reader:
+                # retrieve row and column info - well all data
+                # if cur_row != row
+                #    self.layout.append(temp_row)
+                #    cur_row = row
+                print(row)
+                x = Key(*row)
+                print(x)
+
+
+def main():
+    x = keyboard('English.csv')
+    x.read_config()
+
+
+if __name__ == "__main__":
+    main()
