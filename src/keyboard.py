a9e5fdd01a916ec346ce2c15a57ee670b5ae4872
glebite <glebite@gmail.com>
2021-10-11 10:09:37 -0400

    Cleanup and a small refactor moment of goodness.

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,30 +1,46 @@
+"""
+"""
 from key import Key
 import csv
 
 
 class keyboard(object):
+    """
+    """
     def __init__(self, key_file):
+        """
+        """
         self.key_file = key_file
         self.layout = list()
 
-    def read_config(self):
+    def process_rows(self, reader):
+        """process_rows - process rows of CSV data and populate the keyboard
+        """
         temp_row = list()
         cur_row = -1
+        for row in reader:
+            key = Key(*row)
+            if key.row != cur_row:
+                self.layout.append(temp_row)
+                temp_row = list()
+                cur_row = key.row
+            else:
+                temp_row.append(key)
+        self.layout.append(temp_row)
+
+    def read_config(self):
+        """
+        """
         with open(self.key_file, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='\'')
-            for row in reader:
-                # retrieve row and column info - well all data
-                # if cur_row != row
-                #    self.layout.append(temp_row)
-                #    cur_row = row
-                print(row)
-                x = Key(*row)
-                print(x)
+            self.process_rows(reader)
 
 
 def main():
     x = keyboard('English.csv')
-    x.read_config()
+    for row in x.read_config():
+        if len(row):
+            print(row[0])
 
 
 if __name__ == "__main__":
