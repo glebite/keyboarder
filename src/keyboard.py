e9a27a1c649cbcc719a3ec2169915229e48c59f7
glebite <glebite@gmail.com>
2021-10-12 20:36:09 -0400

    Updating lots of stuff - it's coming along.

--- src/keyboard.py
+++ src/keyboard.py
@@ -5,7 +5,7 @@ import csv
 
 
 class keyboard(object):
-    """
+    """keyboard - collection of keys for a given keyboard
     """
     def __init__(self, key_file):
         """
@@ -14,6 +14,7 @@ class keyboard(object):
         self.layout = list()
         self.position = dict()
         self.pos_map = dict()
+        self.rows = 0
 
     def process_rows(self, reader):
         """process_rows - process rows of CSV data and populate the keyboard
@@ -21,12 +22,11 @@ class keyboard(object):
         temp_row = list()
         cur_row = -1
         for row in reader:
-            print(row)
             key = Key(*row)
             self.position[key.upper] = (key.row, key.column)
             self.position[key.lower] = (key.row, key.column)
             self.pos_map[(key.row, key.column)] = {key.lower, key.upper}
-            
+
             if key.row != cur_row:
                 self.layout.append(temp_row)
                 temp_row = list()
@@ -34,6 +34,7 @@ class keyboard(object):
             else:
                 temp_row.append(key)
         self.layout.append(temp_row)
+        self.rows = key.row
 
     def read_config(self):
         """
@@ -43,7 +44,10 @@ class keyboard(object):
             self.process_rows(reader)
 
     def get_key_position(self, character):
-        return self.position[character]
+        if character in self.position:
+            return self.position[character]
+        else:
+            return (-1, -1)
 
     def get_char_from_position(self, row, column):
         return self.pos_map[(row, column)]
@@ -53,7 +57,7 @@ def main():
     x = keyboard('Farsi.csv')
     x.read_config()
 
-    print(x.get_key_position('ت'))
+    print(x.get_key_position('x'))
     print(x.get_char_from_position(3, 3))
 
 
---
+++ src/layout2.py
@@ -0,0 +1,24 @@
+import keyboard
+import curses
+
+
+class Layout(object):
+    def __init__(self, keyboard_file):
+        self.keyboard_file = keyboard_file
+        self.keyboard = keyboard.keyboard(self.keyboard_file)
+        self.keyboard.read_config()
+
+    def dump_keyboard(self):
+        for row in self.keyboard.layout:
+            for key in row:
+                print(f'{key.lower} ', end='')
+            print('')
+
+
+def main():
+    x = Layout('English.csv')
+    x.dump_keyboard()
+
+
+if __name__ == "__main__":
+    main()
