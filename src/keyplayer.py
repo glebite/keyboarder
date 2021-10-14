39a806b08c49e454ae9b0f8102f28c6a906cd8db
glebite <glebite@gmail.com>
2021-10-13 21:00:39 -0400

    keyplayer is a POC

---
+++ src/keyplayer.py
@@ -0,0 +1,22 @@
+import keyboard
+import curses
+
+
+def main():
+    f = keyboard.keyboard('Farsi.csv')
+    e = keyboard.keyboard('English.csv')
+    e.read_config()
+    f.read_config()
+    stdscr = curses.initscr()
+    curses.cbreak()
+    stdscr.keypad(1)
+
+    key = ''
+    while key != ord('q'):
+        key = stdscr.getch()
+        row, column = e.get_key_position(chr(key))
+        print(f.get_char_from_position(row, column))
+
+
+if __name__ == "__main__":
+    main()
