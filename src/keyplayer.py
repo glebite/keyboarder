342987dcb17f8d42a33ef236715bf073fa8a9b57
glebite <glebite@gmail.com>
2021-10-17 07:57:10 -0400

    Rolling up sample code into a class

--- src/keyplayer.py
+++ src/keyplayer.py
@@ -5,27 +5,32 @@ import curses
 
 
 class KeyPlayer(object):
-    def __init__(self):
-        pass
+    def __init__(self, target_kbd, host_kbd):
+        self.target = keyboard.keyboard(target_kbd)
+        self.host = keyboard.keyboard(host_kbd)
+
+    def sample_test(self):
+        self.host.read_config()
+        self.target.read_config()
+        stdscr = curses.initscr()
+        curses.cbreak()
+        stdscr.keypad(1)
+
+        key = ''
+        while key != ord('q'):
+            key = stdscr.getch()
+            row, column = self.host.get_key_position(chr(key))
+            lower, upper = self.target.get_char_from_position(row, column)
+            stdscr.addch(upper)
+
+        curses.endwin()
 
 
 def main(lang1, lang2):
     """
     """
-    target = keyboard.keyboard(lang1)
-    host = keyboard.keyboard(lang2)
-    host.read_config()
-    target.read_config()
-    stdscr = curses.initscr()
-    curses.cbreak()
-    stdscr.keypad(1)
-
-    key = ''
-    while key != ord('q'):
-        key = stdscr.getch()
-        row, column = host.get_key_position(chr(key))
-        lower, upper = target.get_char_from_position(row, column)
-        stdscr.addch(upper)
+    x = KeyPlayer(lang1, lang2)
+    x.sample_test()
 
 
 if __name__ == "__main__":
