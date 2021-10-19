3ebce3484433c223c9ac0f7303c6b6730fb5ed7c
glebite <glebite@gmail.com>
2021-10-19 06:58:06 -0400

    Adding in the concept of game stats.

--- src/keyplayer.py
+++ src/keyplayer.py
@@ -4,11 +4,12 @@ import keyboard
 import curses
 
 
-
 class KeyPlayer(object):
     def __init__(self, target_kbd, host_kbd):
         self.target = keyboard.keyboard(target_kbd)
         self.host = keyboard.keyboard(host_kbd)
+        self.key_correct = 0
+        self.key_presses = 0
 
     def sample_test(self):
         """
@@ -23,14 +24,19 @@ class KeyPlayer(object):
         while key != ord('q'):
             key = stdscr.getch()
             row, column = self.host.get_key_position(chr(key))
+            print(row, column)
             lower, upper = self.target.get_char_from_position(row, column)
             try:
                 stdscr.addch(lower)
+                stdscr.addch(upper)
             except Exception as e:
                 # yeah - raw exception for now.
-                pass
-
+                print(e)
         curses.endwin()
+
+    def clear_score(self):
+        self.key_presses = 0
+        self.key_correct = 0
 
 
 def main(lang1, lang2):
