eaafc20f1233aaaff4daefb6e3d4487b354d6200
glebite <glebite@gmail.com>
2021-10-23 07:30:27 -0400

    Moar tests for Ba'al!

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -9,3 +9,21 @@ def test_keyboard_creation():
 def test_read_config():
     board = Keyboard('src/Farsi.csv')
     board.read_config()
+
+
+def test_get_key_position():
+    board = Keyboard('src/English.csv')
+    board.read_config()
+    assert board.get_key_position('1') == (0, 1)
+
+
+def test_get_bad_key_position():
+    board = Keyboard('src/English.csv')
+    board.read_config()
+    assert board.get_key_position(' ') == (-1, -1)
+
+
+def test_get_char_from():
+    board = Keyboard('src/English.csv')
+    board.read_config()
+    assert board.get_char_from_position(0, 1) == {'!', '1'}
