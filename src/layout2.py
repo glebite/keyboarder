9232e58772a7ff0c09d8bd75e3ffa9c49cb028f9
glebite <glebite@gmail.com>
2021-10-27 06:53:17 -0400

    Adding in unicode characters for box outline of keys.

--- src/layout2.py
+++ src/layout2.py
@@ -2,6 +2,19 @@ import kboard
 import curses
 
 
+UPPER_LEFT_CORNER = u'\u250f'
+UPPER_RIGHT_CORNER = u'\u2513'
+LOWER_LEFT_CORNER = u'\u2517'
+LOWER_RIGHT_CORNER = u'\u251b'
+HORIZONTAL_BAR = u'\u2501'
+VERTICAL_BAR = u'\u2503'
+CROSS_BAR = u'\u254b'
+LEFT_VERTICAL_T = u'\u2523'
+RIGHT_VERTICAL_T = u'\u252b'
+HORIZONTAL_DOWN_T = u'\u2533'
+HORIZONTAL_UP_T = u'\u253b'
+
+
 class Layout(object):
     def __init__(self, keyboard_file):
         self.keyboard_file = keyboard_file
