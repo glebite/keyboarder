ae4614430446fe73c4e03a673514b9c10c2f8dc3
glebite <glebite@gmail.com>
2021-11-13 08:37:33 -0500

    More testing fun!

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,4 +1,7 @@
-from layout2 import Layout
+# from layout2 import Layout
+# import layout2
+from layout2 import Layout, UPPER_LEFT_CORNER, UPPER_RIGHT_CORNER,\
+    HORIZONTAL_BAR, VERTICAL_BAR, LOWER_LEFT_CORNER, LOWER_RIGHT_CORNER
 
 
 def test_layout_creation():
@@ -31,3 +34,13 @@ def test_position_calc_unknown():
     board = Layout('src/English.csv')
     row, col = board.calculate_position('ب')
     assert row == -1 and col == -1, f'row {row} != {-1} and col {col} != {-1}'
+
+
+def test_boxit():
+    board = Layout('src/English.csv')
+    result = board.boxit('a')
+    constructed = UPPER_LEFT_CORNER + HORIZONTAL_BAR +\
+        UPPER_RIGHT_CORNER + '\n' +\
+        VERTICAL_BAR + 'a' + VERTICAL_BAR + '\n' +\
+        LOWER_LEFT_CORNER + HORIZONTAL_BAR + LOWER_RIGHT_CORNER + '\n'
+    assert result == constructed
