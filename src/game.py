8abf531889475881536dbb6cbe167b59c7d4599f
glebite <glebite@gmail.com>
2021-11-15 18:34:51 -0500

    Fixed the game reading and data setting.

--- src/game.py
+++ src/game.py
@@ -11,17 +11,18 @@ class Game:
         self.game_file = game_file
         self.data = {'name': None,
                      'description': None,
-                     'characters': True,
-                     'hints': True,
-                     'number': 10,
-                     'words': False,
-                     'timed': False,
+                     'characters': None,
+                     'timed': None,
+                     'hints': None,
+                     'number': None,
+                     'words': None,
                      'word_file': None,
                      'host_kbd': None,
-                     'target_kdb': None}
+                     'target_kbd': None}
 
     def load_game(self):
-        self.cfg_parser = configparser.ConfigParser(converters={"any": lambda x: literal_eval(x)})
+        self.cfg_parser = configparser.\
+            ConfigParser(converters={"any": lambda x: literal_eval(x)})
         try:
             self.cfg_parser.read_file(open(self.game_file, 'r'))
         except FileNotFoundError:
@@ -30,12 +31,22 @@ class Game:
         except Exception as e:
             print(f'Unexpected exception raised: {e}')
             raise e
-        self._configure_game()
+        try:
+            self._configure_game()
+        except Exception as e:
+            print(e)
 
     def _configure_game(self):
         for entry in self.cfg_parser['Game']:
-            self.data[entry] = self.cfg_parser['Game'][entry]
-        print(self.data)
+            if self.cfg_parser['Game'][entry].isdigit():
+                self.data[entry] =\
+                    int(self.cfg_parser['Game'][entry])
+            elif self.cfg_parser['Game'][entry] in ['True', 'False']:
+                self.data[entry] =\
+                    bool(self.cfg_parser['Game'][entry] is True)
+            else:
+                self.data[entry] =\
+                    self.cfg_parser['Game'][entry]
 
     def run(self):
         success = 0
@@ -54,7 +65,7 @@ class Game:
         player.host_layout.screen.addstr(9, 55, 'Fail:    ')
         player.host_layout.screen.refresh()
 
-        for game_round in range(int(self.data['number'])):
+        for game_round in range(self.data['number']):
             target_character = player.target.pick_random_key()
 
             if len(target_character) > 3 or target_character in keys_to_ignore:
@@ -63,12 +74,15 @@ class Game:
                 keyboard.get_key_position(target_character)
             host_char = player.host_layout.keyboard.\
                 get_char_from_position(row, column)[0]
-            player.host_layout.key_visibility(host_char, state='ON')
+            if self.data['hints']:
+                player.host_layout.key_visibility(host_char, state='ON')
+
             player.host_layout.screen.addstr(5, 60, target_character)
             player.host_layout.screen.refresh()
 
             in_key = chr(player.host_layout.screen.getch())
-            player.host_layout.key_visibility(host_char, state='OFF')
+            if self.data['hints']:
+                player.host_layout.key_visibility(host_char, state='OFF')
 
             if in_key == host_char:
                 success += 1
