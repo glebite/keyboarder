13e2b806eb0bb2f88b8dc6be65397c4635ecdb88
glebite <glebite@gmail.com>
2021-11-14 19:21:03 -0500

    Expanded game definition, tested load capabilities.

--- src/game.py
+++ src/game.py
@@ -6,6 +6,16 @@ class Game:
     """
     def __init__(self, game_file):
         self.game_file = game_file
+        self.data = {'name': None,
+                     'description': None,
+                     'characters': True,
+                     'hints': True,
+                     'number': 10,
+                     'words': False,
+                     'timed': False,
+                     'word_file': None,
+                     'host_kbd': None,
+                     'target_kdb': None}
 
     def load_game(self):
         self.cfg_parser = configparser.ConfigParser()
@@ -15,5 +25,19 @@ class Game:
             print(f'Sorry - file not found... {self.game_file}')
             raise FileNotFoundError
         except Exception as e:
-            print(f'Exception raised: {e}')
-        print(self.cfg_parser.sections)
+            print(f'Unexpected exception raised: {e}')
+            raise e
+        self._configure_game()
+
+    def _configure_game(self):
+        for entry in self.cfg_parser['Game']:
+            self.data[entry] = self.cfg_parser['Game'][entry]
+        print(self.data)
+
+
+def main():
+    g = Game('../data/game_1.cfg')
+    g.load_game()
+
+
+if __name__ == "__main__": main()
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,4 +1,5 @@
 from game import Game
+import configparser
 
 
 def test_game_creation():
@@ -23,3 +24,21 @@ def test_game_missing_config():
         assert False
     except FileNotFoundError:
         assert True
+
+
+def test_game_invalid_config_file():
+    game = Game('README.md')
+    try:
+        game.load_game()
+        assert False, "README.md is not a config file and should have" \
+            " thrown an exception - MissingSectionHeaderError."
+    except configparser.MissingSectionHeaderError:
+        assert True
+
+
+def test_load_game():
+    game = Game('data/game_1.cfg')
+    game.load_game()
+    assert game.data['target_kbd'] == 'Farsi_RTL.csv', f"target_kbd" \
+        f" is {game.data['target_kbd']} instead of Farsi_RTL.csv"
+
