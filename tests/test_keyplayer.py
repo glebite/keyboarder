fa0962e3465c16a0026e98176a41c66b0e455dfe
glebite <glebite@gmail.com>
2021-12-01 06:42:44 -0500

    Checking in test deficit that should have been caught if there was a test for the code changes

--- tests/test_keyplayer.py
+++ tests/test_keyplayer.py
@@ -7,3 +7,10 @@ ENGLISH = 'src/English.csv'
 def test_keyplayer_creation():
     kp = KeyPlayer(FARSI, ENGLISH)
     assert kp, 'Could not create a KeyPlayer object'
+
+
+def test_keyplayer_pick_random():
+    kp = KeyPlayer(FARSI, ENGLISH)
+    target, host = kp.from_target_pick_host()
+    assert target, f'Target not created: {target}'
+    assert host, f'Host not created: {host}'
