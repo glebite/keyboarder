5fb1275784606c23e1a94de91fbb547494b4fdb5
glebite <glebite@gmail.com>
2021-12-14 06:09:07 -0500

    Testing out reading from server.

--- src/textclient.py
+++ src/textclient.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 
 class TextClient:
@@ -6,3 +7,13 @@ class TextClient:
         if ':' not in server_ip:
             raise ValueError
         self.server_ip = server_ip
+        self.player = None
+
+    def get_key_data(self):
+        r = requests.get(self.server_ip + '/pick_key')
+        print(r.status_code)
+
+       
+if __name__ == "__main__":
+    tc = TextClient(sys.argv[1])
+    tc.get_key_data()
